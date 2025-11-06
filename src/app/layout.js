6b071c64f7c33bb71f8defe04444628fc2004b6e
JoDe <jodejpg@gmail.com>
2025-11-06 08:42:53 +0100

    init

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata = {
+  title: "FamilyFriends",
+  description: "A platform to connect with a family's best friends.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
